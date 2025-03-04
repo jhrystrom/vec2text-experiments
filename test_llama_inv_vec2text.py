fdd446a502978cfa8e9f7b8d01c7a349c463fc4c
jhrystrom <jhvithamar@gmail.com>
2025-03-04 16:54:45 +0000

    add pyproject.toml

--- test_llama_inv_vec2text.py
+++
@@ -1,6 +0,0 @@
-import vec2text
-
-device = "cuda"
-inversion_model=vec2text.models.InversionFromLogitsEmbModel.from_pretrained("jxm/t5-base__llama-7b__one-million-instructions__emb").to(device)
-corrector_model = vec2text.models.CorrectorEncoderFromLogitsModel.from_pretrained("jxm/t5-base___llama-7b___one-million-instructions__correct")
-corrector = vec2text.load_corrector(inversion_model,corrector_model)
